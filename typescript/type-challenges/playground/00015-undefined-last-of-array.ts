25e0e16a838e1fb56e5fc4862631805d01aecd6e
RafaZeero <rafaa99@gmail.com>
2023-01-26 22:05:21 -0300

    solve: (easy) last of array

--- typescript/type-challenges/playground/00015-undefined-last-of-array.ts
+++ typescript/type-challenges/playground/00015-undefined-last-of-array.ts
@@ -24,15 +24,12 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Last<T extends any[]> = any
+type Last<TArray extends any[]> = TArray extends [...any[], infer TLast] ? TLast : never;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
 
-type cases = [
-  Expect<Equal<Last<[3, 2, 1]>, 1>>,
-  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
-]
+type cases = [Expect<Equal<Last<[3, 2, 1]>, 1>>, Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>];
 
 /* _____________ Further Steps _____________ */
 /*
